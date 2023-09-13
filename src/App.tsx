0b3a835e04bbe5d0679a84a61c345ee49f5f8167
Aziz Halilov <poplyq@live.com>
2023-09-13 16:07:56 +0300

    add normalise and scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
 import AppRouter from './routes/AppRouter'
+import { Header } from './modules/header/Header'
 
 function App() {
   return (
     <div className='App'>
+      <Header />
       <AppRouter />
     </div>
   )
---
+++ src/modules/header/Header.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import './header.scss'
+
+export const Header = () => {
+  return <div className='header'></div>
+}
